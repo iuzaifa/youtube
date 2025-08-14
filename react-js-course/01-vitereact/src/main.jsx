f32cbfc6b19e46b562942d0d1d20681510afc689
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-08-15 01:45:21 +0530

    Day 2 : update react 02-counter

--- react-js-course/01-vitereact/src/main.jsx
+++ react-js-course/01-vitereact/src/main.jsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import App from './App'
+
+
+function MyApp() {
+
+  return (
+    <>
+      <h1>Hello Custom My App!.............</h1>
+      <p>I am Abu Huzaifa The writer of the code</p>
+    </>
+  )
+}
+
+const reactElement = React.createElement(
+  'a',
+  {href : 'https://google.com', target : '_blank'},
+  'click me'
+)
 
 createRoot(document.getElementById('root')).render(
+
+
+
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <App/>
+    
+  </StrictMode>
+
+  // reactElement
+  // MyApp()
+
+
 )
