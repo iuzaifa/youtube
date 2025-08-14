b81128e1923da92e5d904f629107f8816468974b
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-08-15 02:54:07 +0530

    Day 2 : update react 02-counter

--- react-js-course/02-counter/src/App.jsx
+++ react-js-course/02-counter/src/App.jsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    // counter = counter + 1;
+    // setCounter(counter) 
+    if(counter == 20 ) return;
+    setCounter(counter + 1);
+  }
+
+
+  const removeValue = () => {
+    if (counter == 0) return;
+    else setCounter(counter - 1);
+  }
+
+  // new way to increase value
+  const rValue = () => {
+    setCounter(prevCounter => prevCounter + 1)
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Chai or Code </h1>
+      <h2>Counter {counter}</h2>
+      <div className='d-flex'>
+        <button onClick={addValue}>Add Value </button>
+        <button onClick={removeValue}>Remove Value </button>
+        <button onClick={rValue}>add Value </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
